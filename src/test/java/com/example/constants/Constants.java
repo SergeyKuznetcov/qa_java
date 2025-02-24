f14e6d61ee5d78eb655958f3022ce3886390ac6f
Kuznetsov Sergey <kuznecovsa96@yandex.ru>
2025-02-24 18:10:44 +0300

    complete main task

---
+++ src/test/java/com/example/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.example.constants;
+
+public class Constants {
+    public static final String FELINE_FAMILY = "Кошачьи";
+    public static final int DEFAULT_FELINE_KITTENS_COUNT = 1;
+    public static final String FELINE_KIND = "Хищник";
+    public static final int CUSTOM_FELINE_KITTENS_COUNT = 3;
+
+    public static final String LION_DEFAULT_SEX = "Самец";
+
+    public static final String CAT_SOUND = "Мяу";
+}
